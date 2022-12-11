3f361920857966d40732d5962302813bc09b800a
MeowDevelopment <ameny.ferchichi99@gmail.com>
2022-12-11 19:01:42 +0100

    new test

--- sum.js
+++ sum.js
@@ -1,4 +1,4 @@
-function sum(a, b) {
-    return a + b
+function diff(a, b) {
+    return a - b
 }
-module.exports = sum
+module.exports = diff
--- sum.test.js
+++ sum.test.js
@@ -1,7 +1,7 @@
-const sum = require("./sum")
-describe('test sum', () => {
-    test('test 1 + 2 = 3', () => {
-        expect(sum(1,2)).toBe(3)
+const diff = require("./sum")
+describe('test diff', () => {
+    test('test 1 - 2 = -1', () => {
+        expect(diff(1,2)).toBe(-1)
         
     });
     
